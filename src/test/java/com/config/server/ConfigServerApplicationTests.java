d5949e21fbf37301b092c5a3f78b194082472c37
ajynky <ajynky.joshi@gmail.com>
2023-02-12 22:33:32 +0530

    second commit

---
+++ src/test/java/com/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.config.server;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	/*
+	 * @Test void contextLoads() { }
+	 */
+
+}
